e4f33c5dde49819caf22be86a09f176b5f129825
UBFSoft <UBFSoft@DESKTOP-H74FKEL>
2022-05-02 18:40:39 +0300

    Add project files.

---
+++ BirthdayApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace BirthdayApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult InvitationForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InvitationForm(InvitationModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                Database.Add(model);
+                return View("Thanks",model);
+            }
+            return View(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Attendees()
+        {
+            return PartialView(Database.List.Where(i =>i.JoiningCase == true));
+        }
+    }
+}
